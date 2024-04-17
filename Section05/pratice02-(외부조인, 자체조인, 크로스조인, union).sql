373d198fd6c7e91b4b85b71869764bd0a8dbb491
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-18 00:22:20 +0900

    Update pratice02-(외부조인, 자체조인, 크로스조인, union).sql

--- Section05/pratice02-(외부조인, 자체조인, 크로스조인, union).sql
+++ Section05/pratice02-(외부조인, 자체조인, 크로스조인, union).sql
@@ -19,7 +19,75 @@ order by u.userid;
 -- right outer join은 left outer join에서 테이블에 자리만 바꿔주면 되는 것이다.
 select u.userid, u.username, b.prodname, u.addr,
 	concat(u.mobile1, u.mobile2) as '연락처'
-from buytbl b
-right outer join usertbl u
-on u.userid = b.userid
-order by u.userid;
+	from buytbl b
+	right outer join usertbl u
+	on u.userid = b.userid
+	order by u.userid;
+
+-- outer join을 이용하여 구매내역이 없는 고객 리스트를 출력해보자.
+select u.userid, u.username, b.prodname, u.addr,
+	concat(u.mobile1, u.mobile2) as '연락처'
+	from buytbl b
+	right outer join usertbl u
+	on u.userid = b.userid
+	where b.prodname is null
+	order by u.userid;
+    
+-- 아래 3개 테이블을 이용하여 학생중에 동아리에 가입하지 않은
+-- 사람만 추출해내는 쿼리문을 작성해보자
+select * from stdtbl;
+select * from stdclubtbl;
+
+select s.stdname, s.addr, c.clubname, c.roomnumber
+	from stdtbl s
+    left outer join stdclubtbl sc
+    on s.stdname = sc.stdname
+    left outer join clubtbl c
+    on sc.clubname = c.clubname
+    where c.clubname is null
+    order by s.stdname;
+
+-- 아래 쿼리는 left outer join과 right outer join을 이용해서 3개의 테이블을
+-- 엮어서 학생이 한명도 가입하지 않은 동아리를 추출하는 쿼리문이다.
+-- 왜 right outer join을 사용했는가? 동아리 명을 다 추출하기 위해서
+select c.clubname, c.roomnumber, s.stdname, s.addr
+	from stdtbl s
+    left outer join stdclubtbl sc
+    on s.stdname = sc.stdname
+    right outer join clubtbl c
+    on sc.clubname = c.clubname
+    where s.stdname is null
+    order by c.clubname;
+
+-- 그런데 위의 두 개의 결과값을 동시에 보고 싶을때는 어떻게 할까?
+-- 두 쿼리의 결과값을 연결해주는 키워드가 필요하다
+-- 그게 바로 union이다. union은 합집합니다. 중복은 제거를 한다.
+select c.clubname, c.roomnumber, s.stdname, s.addr
+	from stdtbl s
+    left outer join stdclubtbl sc
+    on s.stdname = sc.stdname
+    right outer join clubtbl c
+    on sc.clubname = c.clubname
+    where s.stdname is null
+union 
+select s.stdname, s.addr, c.clubname, c.roomnumber
+	from stdtbl s
+    left outer join stdclubtbl sc
+    on s.stdname = sc.stdname
+    left outer join clubtbl c
+    on sc.clubname = c.clubname
+    where c.clubname is null;
+
+-- union : 중복제거
+-- union all : 중복을 제거하지 않고 다 출력한다.
+select * from stdtbl
+union all
+select * from stdtbl;
+
+-- 상호 조인에 대해서 알아보자
+-- cross join의 결과 개수는 두 테이블의 행의 개수를 곱하여 나온 개수가 된다.
+-- 카티션 곱(Cartition Product)이라고도 부른다. 통상적으로 대용량 데이터를 생성할 때 사용한다.
+-- buytbl : 13개의 행, usertbl : 10개의 행
+select *
+	from buytbl
+cross join usertbl;
