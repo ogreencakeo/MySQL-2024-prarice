fe9a2e60688f3f65d09403d2bb6bb240c56ba940
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-05-03 08:34:43 +0900

    Update 실습-1(뷰).sql

--- Section08/실습-1(뷰).sql
+++ Section08/실습-1(뷰).sql
@@ -3,7 +3,8 @@ use sqldb;
 
 -- 아래 코드는 view를 만드는 쿼리이다. 설명을 하자면 v_userbuytbl을
 -- 만드는데 select문의 내용을 view로 만들겠다라는 것이다.
-
+-- 매번 아래와 같이 조회를 하게 되면 지속적으로 쿼리를 길게 쳐야될 것이다.
+-- 한 번 만들고 view를 적절히 잘 활용을 하면 될 것이다.
 create view v_userbuytbl
 as
 	select U.userid as 'USER_ID', U.username as 'USER_NAME',
@@ -12,3 +13,6 @@ as
     from usertbl U
     inner join buytbl B
     on U.userid = B.userid;
+
+select *
+	from v_userbuytbl;
