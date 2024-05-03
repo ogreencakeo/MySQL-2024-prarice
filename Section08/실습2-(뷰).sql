99a1fa84cb1e04d7a09a51e2f6dca918664fd7c2
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-05-03 22:49:28 +0900

    Update 실습2-(뷰).sql

--- Section08/실습2-(뷰).sql
+++ Section08/실습2-(뷰).sql
@@ -48,6 +48,39 @@ select * from usertbl;
 -- 삽입은 된다. 하지만, 뷰를 조회를 해보면 나오지 않는다.
 -- 왜? 뷰가 177이상만 조회를 하니까 나오지 않는다.
 insert into v_height177 values('SEH', '신은혁', 2008, '구미', NULL, NULL, 140, '2010-05-05');
+insert into v_height177 values('KCS', '김철수', 2000, '안양', NULL, NULL, 180, '2002-05-05');
 select * from v_height177;
 
 select * from usertbl;
+
+-- 근데 혼란이 온다. 177이상인 데이터만 입력을 받기 위해서 with check option구문을 
+-- 사용하면 된다.
+alter view v_height177
+as
+	select * from usertbl
+    where height >= 177
+    with check option; -- 177 이상인지 체크를 하는 구문이다.
+
+insert into v_height177 values('KKK', '김기군', 2008, '구미', NULL, NULL, 140, '2010-05-05');
+
+-- 복합뷰(조인뷰)도 아래와 같이 만들 수 있다.
+create or replace view v_userbuytbl
+as
+	select U.userid as 'USER ID', U.username as 'USER NAME',
+    B.prodname as 'PROD NAME', U.addr as '주소'
+	from usertbl U
+    inner join buytbl B
+    on U.userid = B.userid;
+
+-- 복합뷰(조인뷰)에 데이터 삽입이 안된다.
+insert into v_userbuytbl values ('PKL', '박경림', '경기');
+
+-- 테이블을 제거했다. 그럼 뷰는 어떻게 되는가 ? 
+drop table buytbl, usertbl;
+
+-- 뷰가 참조하고 있던 실체(테이블)가 제거되었으니 뷰는 실행이 되지 않는다.
+select * from v_userbuytbl;
+
+-- 이 때는 뷰가 참조하고 있는 테이블이 어떤 것인지 아래와 같이 확인을 해보면
+-- 명확하게 알 수가 있다.
+check table v_userbuytbl;
