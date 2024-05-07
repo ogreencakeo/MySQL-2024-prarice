ed21beb50c6e190447b39740a450a4feaa9b5bc1
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-05-08 00:39:14 +0900

    Update pratice02(인덱스 성능비교-2).sql

--- Section09/pratice02(인덱스 성능비교-2).sql
+++ Section09/pratice02(인덱스 성능비교-2).sql
@@ -29,3 +29,67 @@ show global status like 'innodb_pages_read';
 
 -- 이로써 인덱스가 있고 없고에 따라 DB의 성능이 엄청나게 차이가 나는 것을 
 -- 알 수가 있다.
+
+-- 이제는 범위 검색을 해보자.
+
+-- 인덱스가 없는 emp를 실행하면 역시 풀테이블 스캔을 하여 약 1030페이지
+-- 를 읽는 것을 알 수가 있다.
+-- 30995.90(query cost)
+show global status like 'innodb_pages_read';
+select * from emp 
+	where emp_no < 11000;
+show global status like 'innodb_pages_read';
+
+-- 읽은 페이지 수 : 7페이지
+-- 201.93(query cost)
+show global status like 'innodb_pages_read';
+select * from emp_c 
+	where emp_no < 11000;
+show global status like 'innodb_pages_read';
+
+-- 읽은 페이지 수 : 900페이지
+-- 1055.94(query cost)
+show global status like 'innodb_pages_read';
+select * from emp_se
+	where emp_no < 11000;
+show global status like 'innodb_pages_read';
+
+-- 위와 같이 확인해 본 결과 범위검색에서는 클러스터형 인덱스 단연 성능이
+-- 훨씬 뛰어나다라는 사실을 확인해 볼 수가 있다.
+
+-- 하지만, where 절이 없다면 어떨까?
+-- where 절이 없으니 풀테이블 스캔을 하여 약 928페이지를 읽는 것을 알 수가 있다.
+show global status like 'innodb_pages_read';
+select * from emp_c; 
+	-- where emp_no < 11000;
+show global status like 'innodb_pages_read';
+
+-- 보조 인덱스가 있는 emp_se의 실행결과는 1038페이지를 읽고
+-- 풀테이블 스캔을 했다.
+show global status like 'innodb_pages_read';
+select * from emp_se; 
+	-- where emp_no < 11000;
+show global status like 'innodb_pages_read';
+
+-- 하여 위의 결론은 범위 검색에서는 클러스터령 인덱스가 가장 성능이 좋다는 것을
+-- 알 수가 있다.
+
+-- 하지만, mysql이 알아서 검색해줄 수도 있다.
+-- emp_se에는 분명 보조 인덱스가 있다. 하지만 실행 계획을 보면 아래 것은
+-- 인덱스를 사용하지 않고 풀테이블 스캔을 했음을 알 수가 있다.
+-- 하여, 보조 인덱스가 있으나 마나 하는 것이니 제거해주는 것이 좋다.
+-- 인덱스 물리적 공간을 차지하고 있으니 말이다.
+-- mysql이 인덱스를 사용할 것인지 풀테이블 스캔을 할 것인지는
+-- 해당 테이블의 약 20프로 이상 스캔하는 경우는 인덱스를 사용하지 않는다.
+-- 전체 데이터 검색을 한다면 차라리 인덱스가 없는 것이 낫다.
+
+show global status like 'innodb_pages_read';
+select * from emp_se 
+	where emp_no < 400000
+    limit 5000000;
+show global status like 'innodb_pages_read';
+
+show global status like 'innodb_pages_read';
+select * from emp_c 
+	where emp_no = 100000;
+show global status like 'innodb_pages_read';
